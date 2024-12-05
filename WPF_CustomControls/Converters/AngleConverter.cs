208dc8dc9e1988123dce089024cf31b99c796745
William Train <William.Train@ametek.com>
2024-12-05 16:31:43 +0000

    Working on an alternate style for BusyIndicator, which looks like an atom with orbiting electrons. Still some work to do but getting closer

---
+++ WPF_CustomControls/Converters/AngleConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_CustomControls.Converters
+{
+    public class AngleConverter : IValueConverter
+    {
+        public double Scale { get; set; } = 1;
+        public double Offset { get; set; } = 0;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return Offset + (d * Scale);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return (d - Offset) / Scale;
+            }
+            return value;
+        }
+    }
+}
